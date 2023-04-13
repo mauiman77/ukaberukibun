e7c61fa7993da6ae47c4179cad38eee25e3bdd62
mauiman77 <boerbasher@protonmail.com>
2023-04-13 20:15:12 +0100

    Added experimental textarea move

--- ukaberukibun_main.js
+++ ukaberukibun_main.js
@@ -1,7 +1,7 @@
 const weeBox = document.querySelector('.toolbox')
 const bigBox = document.querySelector('.toolbox-safearea')
 bigBox.addEventListener('mouseover', () => {
-  weeBox.style.cssText = 'top: 0rem'
+  weeBox.style.cssText = 'left: 0rem'
 })
 
 bigBox.addEventListener('mouseout', () => {
@@ -13,3 +13,12 @@ function newBox () {
   const mainBit = document.querySelector('.editor')
   mainBit.appendChild(textToAdd)
 }
+
+window.addEventListener('click', (e) => {
+  let y = e.clientY
+  let x = e.clientX
+  console.log(y)
+
+  const textBox = document.querySelector('textarea')
+  textBox.style.cssText = `left: ${x}px; top: ${y}px`
+})
